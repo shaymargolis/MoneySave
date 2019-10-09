83fa4603d384ddd38426d68c95e925b49f47b9f3
Shay Maroglis <shay@inbar.co.il>
2019-10-09 15:31:08 +0300

    Move transaction tags to TransactionType

--- models/Transaction.js
+++ models/Transaction.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 
 const transactionSchema = new mongoose.Schema({
   _transaction_type: { type: mongoose.Schema.Types.ObjectId, ref: 'TransactionType' },
-  _transaction_tags: { type: mongoose.Schema.Types.ObjectId, ref: 'TransactionTag' },
   is_outcome: Boolean,
   datetime: Date,
   sum: Number
--- models/TransactionType.js
+++ models/TransactionType.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const transactionTypeSchema = new mongoose.Schema({
   title: String,
+  _transaction_tags: { type: mongoose.Schema.Types.ObjectId, ref: 'TransactionTag' },
   _transactions: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Transaction' }]
 }, {
   timestamps: true
