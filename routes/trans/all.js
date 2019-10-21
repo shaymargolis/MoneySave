8147903cfd668c9810afdcb681b37bb6fe157ed2
Shay Maroglis <shay@inbar.co.il>
2019-10-21 17:37:31 +0300

    Sort transactions by datetime

--- routes/trans/all.js
+++ routes/trans/all.js
@@ -3,13 +3,17 @@ const mongoose = require('mongoose');
 const Transaction = mongoose.model("Transaction");
 
 module.exports = async (req, res) => {
-  Transaction.find().populate({
+  var all = await Transaction.find().populate({
       path: "_transaction_type",
       populate: {
         path: "_transaction_tag",
       }
-    }).exec(function(err, all) {
-    res.render("trans/all", { trans_list: all, action: req.query.action, result: req.query.result })
-  });
+    }).sort({"datetime": -1});
 
+  res.render("trans/all", {
+    trans_list: all,
+    action: req.query.action,
+    result: req.query.result,
+    moment: require('moment')
+  })
 };
