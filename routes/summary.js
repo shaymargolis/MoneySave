c3cdf060db395b64405d2c9c3f42c5d94a33ea06
Shay Maroglis <shay@inbar.co.il>
2019-10-21 13:55:14 +0300

    Show basic spending info in summary

--- routes/summary.js
+++ routes/summary.js
@@ -1,28 +1,50 @@
-
+const moment = require('moment');
 const mongoose = require('mongoose');
 
 const Transaction = mongoose.model("Transaction");
+const TransactionType = mongoose.model("TransactionType");
 const TransactionTag = mongoose.model("TransactionTag");
 
 module.exports = async (req, res) => {
   var tags = await TransactionTag.find();
-  var sum_tags = [];
+  var sum_tags = {};
+  var total_spent = 0;
 
   for (let tag of tags) {
-    var transactions = await Transaction.find().populate({
-      path: "_transaction_type",
-      match: {
-        '_transaction_tag.title': tag
-      }
+    var trans_types = await TransactionType.find({
+      _transaction_tag: tag._id
     });
 
+    var transactions = await Transaction.find({
+      datetime: {
+        $gte: moment().startOf('month').toDate(),
+        $lte: moment().endOf('month').toDate()
+      },
+      _transaction_type: trans_types
+    });
 
+    sum_tags[tag.title] = {sum: 0, color: tag.color};
+
+    for (let trans of transactions) {
+      sum_tags[tag.title].sum += trans.sum;
+    }
+
+    total_spent += sum_tags[tag.title].sum;
+  }
+
+  var tag_values = Object.keys(sum_tags);
+  var tag_colors = [];
+  var tag_sums = [];
+
+  for (let tag of tag_values) {
+    tag_colors.push(sum_tags[tag].color);
+    tag_sums.push(sum_tags[tag].sum);
   }
 
   res.render("summary", {spending_circle: {
-    labels: ["אוכל", "נותר"],
-    colors: ["blue", "gray"],
-    data: [50, 50]
+    labels: tag_values,
+    colors: tag_colors,
+    data: tag_sums
   }
   })
 };
