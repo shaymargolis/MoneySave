d234bcdb77195d510ac714bc788791ffb191e616
Shay Maroglis <shay@inbar.co.il>
2019-10-21 16:33:01 +0300

    Basic looking summary

--- models/TransactionType.js
+++ models/TransactionType.js
@@ -5,7 +5,8 @@ const transactionTypeSchema = new mongoose.Schema({
   is_outcome: Boolean,
   _user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   _transaction_tag: { type: mongoose.Schema.Types.ObjectId, ref: 'TransactionTag' },
-  _transactions: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Transaction' }]
+  _transactions: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Transaction' }],
+  _periodic_transactions: [{ type: mongoose.Schema.Types.ObjectId, ref: 'PeriodicTransaction' }]
 }, {
   timestamps: true
 });
--- routes/summary.js
+++ routes/summary.js
@@ -2,17 +2,21 @@ const moment = require('moment');
 const mongoose = require('mongoose');
 
 const Transaction = mongoose.model("Transaction");
+const PeriodicTransaction = mongoose.model("PeriodicTransaction");
 const TransactionType = mongoose.model("TransactionType");
 const TransactionTag = mongoose.model("TransactionTag");
 
 module.exports = async (req, res) => {
   var tags = await TransactionTag.find();
   var sum_tags = {};
+  var recieve_tags = {};
   var total_spent = 0;
+  var total_recieved = 0;
 
   for (let tag of tags) {
     var trans_types = await TransactionType.find({
-      _transaction_tag: tag._id
+      _transaction_tag: tag._id,
+      is_outcome: false
     });
 
     var transactions = await Transaction.find({
@@ -23,6 +27,41 @@ module.exports = async (req, res) => {
       _transaction_type: trans_types
     });
 
+    var periodic = await PeriodicTransaction.find({
+      _transaction_type: trans_types
+    });
+
+    transactions = transactions.concat(periodic);
+
+    recieve_tags[tag.title] = {sum: 0, color: tag.color};
+
+    for (let trans of transactions) {
+      recieve_tags[tag.title].sum += trans.sum;
+    }
+
+    total_recieved += recieve_tags[tag.title].sum;
+  }
+
+  for (let tag of tags) {
+    var trans_types = await TransactionType.find({
+      _transaction_tag: tag._id,
+      is_outcome: true
+    });
+
+    var transactions = await Transaction.find({
+      datetime: {
+        $gte: moment().startOf('month').toDate(),
+        $lte: moment().endOf('month').toDate()
+      },
+      _transaction_type: trans_types
+    });
+
+    var periodic = await PeriodicTransaction.find({
+      _transaction_type: trans_types
+    });
+
+    transactions = transactions.concat(periodic);
+
     sum_tags[tag.title] = {sum: 0, color: tag.color};
 
     for (let trans of transactions) {
@@ -30,6 +69,10 @@ module.exports = async (req, res) => {
     }
 
     total_spent += sum_tags[tag.title].sum;
+  }
+
+  if (total_recieved > total_spent) {
+    sum_tags["נותר"] = {sum: total_recieved-total_spent, color: "gray"};
   }
 
   var tag_values = Object.keys(sum_tags);
@@ -41,10 +84,12 @@ module.exports = async (req, res) => {
     tag_sums.push(sum_tags[tag].sum);
   }
 
-  res.render("summary", {spending_circle: {
-    labels: tag_values,
-    colors: tag_colors,
-    data: tag_sums
-  }
+  res.render("summary", {
+    remaining_sum: total_recieved-total_spent,
+    spending_circle: {
+      labels: tag_values,
+      colors: tag_colors,
+      data: tag_sums
+    }
   })
 };
