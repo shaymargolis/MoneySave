29432b44651d1b166344e6bcd1d33d2b144b12c3
Shay Maroglis <shay@inbar.co.il>
2019-10-21 18:54:28 +0300

    Dynamic data update from TransactionType in createTransaction

--- routes/ajax/transTypes.js
+++ routes/ajax/transTypes.js
@@ -5,7 +5,7 @@ const TransactionType = mongoose.model("TransactionType");
 module.exports = async (req, res) => {
   var query = req.params.query;
 
-  var result = await TransactionType.find({title: { $regex: '.*' + query + '.*' }});
+  var result = await TransactionType.find({title: { $regex: '.*' + query + '.*' }}).populate("_transaction_tag");
 
   res.status(200).json(result);
 };
